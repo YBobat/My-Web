dc08b84482ce697bfde0a30a995963e34e664888
YBobat <Bobaty8@gmail.com>
2023-05-22 16:20:59 -0700

    Input Feilds + buttons

--- src/main/java/com/example/application/views/list/ContactForms.java
+++ src/main/java/com/example/application/views/list/ContactForms.java
@@ -2,10 +2,17 @@ package com.example.application.views.list;
 
 import com.example.application.data.entity.Company;
 import com.example.application.data.entity.Status;
+import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.Key;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.combobox.ComboBox;
 import com.vaadin.flow.component.formlayout.FormLayout;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.textfield.EmailField;
 import com.vaadin.flow.component.textfield.TextField;
+
+import java.util.List;
 
 public class ContactForms extends FormLayout {
     TextField firstName = new TextField("First Name");
@@ -13,4 +20,40 @@ public class ContactForms extends FormLayout {
     EmailField email = new EmailField("Email");
     ComboBox<Status> status = new ComboBox<>("status");
     ComboBox<Company> company = new ComboBox<>("Company");
+
+    Button save = new Button("save");
+    Button delete = new Button("delete");
+    Button cancel = new Button("cancel");
+
+    public ContactForms(List<Company> companies, List<Status> statuses) {
+        addClassName("contact-form");
+
+        company.setItems(companies);
+        company.setItemLabelGenerator(Company::getName);
+
+        status.setItems(statuses);
+        status.setItemLabelGenerator(Status::getName);
+
+
+        add(
+                firstName,
+                lastName,
+                email,
+                company,
+                status,
+                createButton()
+        );
+
+    }
+
+    private Component createButton() {
+        save.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+        delete.addThemeVariants(ButtonVariant.LUMO_ERROR);
+        save.addThemeVariants(ButtonVariant.LUMO_TERTIARY);
+
+        save.addClickShortcut(Key.ENTER);
+        cancel.addClickShortcut(Key.ESCAPE);
+        return new HorizontalLayout(save, delete, cancel);
+    }
 }
+
