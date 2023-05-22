51f11972ef35e30cb517b25b8df04a44ef887328
YBobat <Bobaty8@gmail.com>
2023-05-22 15:21:33 -0700

    test1

--- src/main/java/com/example/application/views/list/ContactForms.java
+++ src/main/java/com/example/application/views/list/ContactForms.java
@@ -1,6 +1,16 @@
 package com.example.application.views.list;
 
+import com.example.application.data.entity.Company;
+import com.example.application.data.entity.Status;
+import com.vaadin.flow.component.combobox.ComboBox;
 import com.vaadin.flow.component.formlayout.FormLayout;
+import com.vaadin.flow.component.textfield.EmailField;
+import com.vaadin.flow.component.textfield.TextField;
 
 public class ContactForms extends FormLayout {
+    TextField firstName = new TextField("First Name");
+    TextField lastName = new TextField("Last Name");
+    EmailField email = new EmailField("Email");
+    ComboBox<Status> status = new ComboBox<>("status");
+    ComboBox<Company> company = new ComboBox<>("Company");
 }
